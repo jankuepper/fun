b0c3da222d51c281780d862ab6915d90c267451a
jankuepper <janluca0000046@gmail.com>
2024-08-01 13:58:12 +0200

    cors

--- invite-be/index.mjs
+++ invite-be/index.mjs
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 app.use(
   cors({
-    origin: "http://jankuepper.de",
+    origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
