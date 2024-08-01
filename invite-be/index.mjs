a72d5f320d851f9a24aee58e4cb1f59d88ef685d
jankuepper <janluca0000046@gmail.com>
2024-08-01 13:22:02 +0200

    update cors

--- invite-be/index.mjs
+++ invite-be/index.mjs
@@ -5,7 +5,7 @@ import db from './db/conn.mjs'
 const app = express()
 const port = 3000
 app.use(cors({
-  origin: 'http://localhost'
+  origin: 'http://jankuepper.de'
 }))
 
 app.get('/invites', async (req, res) => {
