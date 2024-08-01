01eaeaa22a7608658173322e06a8086085e588d8
jankuepper <janluca0000046@gmail.com>
2024-08-01 13:33:06 +0200

    fixes

--- invite-be/index.mjs
+++ invite-be/index.mjs
@@ -1,25 +1,30 @@
-import express from 'express'
-import cors from 'cors'
-import db from './db/conn.mjs'
+import express from "express";
+import cors from "cors";
+import db from "./db/conn.mjs";
 
-const app = express()
-const port = 3000
-app.use(cors({
-  origin: '*'
-}))
+const app = express();
+const port = 3000;
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 
-app.get('/invites', async (req, res) => {
-  let collection = await db.collection("invites")
-  let results = await collection.find({}).toArray()
-  res.send(results)
-})
+app.get("/invites", async (req, res) => {
+  let collection = await db.collection("invites");
+  let results = await collection.find({}).toArray();
+  res.send(results);
+});
 
-app.post('/invites', express.json(), async (req, res) => {
-  let collection = await db.collection("invites")
-  let results = await collection.insertOne(req.body)
-  res.send(results)
-})
+app.post("/invites", express.json(), async (req, res) => {
+  let collection = await db.collection("invites");
+  let results = await collection.insertOne(req.body);
+  res.send(results);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
